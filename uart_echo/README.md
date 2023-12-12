cf5ca71f63ec7db18ff16065a42d568ddc4dedbc
Willy Evans <buffalo@willyevans.net>
2023-12-11 20:00:30 -0700

    Created uart_echo.

--- uart_echo/README.md
+++ uart_echo/README.md
@@ -2,9 +2,9 @@ USART To Do List:
 - ~~Return usart_handle_t~~
 - ~~Create usart_rx and usart_tx tasks.~~
 - Use USART as loopback RX -> TX
-    - Use queue to send data from usart_rx to usart_tx
-    - Blocking RX/TX calls at first.
-    - Therefore echo back on terminal display.
+    - ~~Use queue to send data from usart_rx to usart_tx~~
+    - ~~~Blocking RX/TX calls at first.~~
+    - ~~Therefore echo back on terminal display.~~
 - Maybe change color?
     - '\x1B' is escape sequence, therefore one char.
 - Update FreeRTOSConfig.h
---
+++ uart_echo/main.c
@@ -0,0 +1,65 @@
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+#include "bluepill.h"
+#include "usart.h"
+#include "led.h"
+
+uint32_t uart = 0;
+static QueueHandle_t uart_q;
+
+static void uart_tx(void *args __attribute((unused)))
+{
+	static uint8_t data = 0;
+
+	while(1)
+	{
+		if(xQueueReceive(uart_q, &data, 500) == pdPASS)
+		{
+			while(!usart_get_flag(uart, USART_SR_TXE))
+			{
+				taskYIELD();
+			}
+			usart_send(uart, data);
+			led_toggle();
+		}
+	}
+	return;
+}
+
+static void uart_rx(void *args __attribute((unused)))
+{
+	static uint8_t data = 0;
+
+	while(1)
+	{
+		if(usart_get_flag(uart, USART_SR_RXNE))
+		{
+			data = usart_recv(uart);
+			xQueueSend(uart_q, &data, portMAX_DELAY);
+		}
+		taskYIELD();
+	}
+	return;
+}
+
+int main(void)
+{
+	// Initialize board.
+	board_init();
+	// Initialize LED.
+	led_init();
+	// Set up UART 1
+	uart = usart_init(1, 38400, 8, USART_PARITY_NONE, USART_STOPBITS_1, false);
+	// Set up UART queue.
+	uart_q = xQueueCreate(16,sizeof(uint8_t));
+	// Create FreeRTOS tasks.
+	xTaskCreate(uart_tx, "uart_tx", 100, NULL, configMAX_PRIORITIES - 1, NULL);
+	xTaskCreate(uart_rx, "uart_rx", 100, NULL, configMAX_PRIORITIES - 1, NULL);
+	// Start FreeRTOS scheduler.
+	vTaskStartScheduler();
+	// Will only get here if the scheduler fails to start.
+	while(1);
+	return 0;
+}
