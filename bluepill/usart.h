1d55ec7d9504bfe44e196044324a01d896ae0cf5
Willy Evans <buffalo@willyevans.net>
2023-12-20 06:50:49 -0700

    Removed 'typedef usart_t *usart_handle_t'.

--- bluepill/usart.h
+++ bluepill/usart.h
@@ -9,8 +9,6 @@
 
 // Opaque USART structure (hides the structure from teh user).
 typedef struct usart_t usart_t;
-// Handle (pointer) to USART.
-typedef usart_t *usart_handle_t;
 
 int32_t usart_init(uint32_t usart, uint32_t baudrate, uint32_t databits, uint32_t parity, uint32_t stopbits, bool flowcontrol);
 
