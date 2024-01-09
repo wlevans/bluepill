5c047f7da1177c0122d56879cd158cb8f2b1bdb0
Willy Evans <buffalo@willyevans.net>
2024-01-09 16:45:22 -0700

    Code cleanup.

--- bluepill/usart.c
+++ bluepill/usart.c
@@ -2,6 +2,7 @@
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/nvic.h>
 
 struct usart_t
 {
--- bluepill/usart.h
+++ bluepill/usart.h
@@ -7,20 +7,15 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <libopencm3/stm32/usart.h>
-#include <libopencm3/cm3/nvic.h>
 
 // To do: Define common baud rates.
 
 // Opaque USART structure (hides the structure from teh user).
 typedef struct usart_t usart_t;
 
-//static QueueHandle_t uart_txq;
-//static QueueHandle_t uart_rxq;
-
 int32_t usart_init(uint32_t usart, uint32_t baudrate, uint32_t databits, uint32_t parity, uint32_t stopbits, bool flowcontrol);
 uint32_t usart_puts(uint32_t usart, char * src);
 uint32_t usart_putc(uint32_t usart, char c);
 uint32_t usart_getc(uint32_t usart, char * c);
-//void usart1_isr(void);
 
 #endif /* BLUEPILL_USART */
