574dae0ba5a334f0602f725109671b06c0875d6d
Willy Evans <buffalo@willyevans.net>
2025-03-11 15:51:57 -0600

    Added one wire skeletal code.

---
+++ misc/onewire.c
@@ -0,0 +1,17 @@
+#include "onewire.h"
+#include "uart.h"
+
+static uint8_t readBit(void)
+{
+
+}
+
+uint8_t readByte(void)
+{
+
+}
+
+void writeByte(uint8_t byte)
+{
+
+}
---
+++ misc/onewire.h
@@ -0,0 +1,8 @@
+#ifndef __BLUEPILL_ONEWIRE__
+#define __BLUEPILL_ONEWIRE__
+
+static uint8_t readBit(void);
+uint8_t readByte(void);
+void writeByte(uint8_t byte);
+
+#endif
