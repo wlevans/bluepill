b18d6c6b1eae5760039a1aa124bc283d951e47f1
Willy Evans <buffalo@willyevans.net>
2025-03-11 18:08:29 -0600

    Updated FreeRTOSConfig.h files.

--- i2c/include/FreeRTOSConfig.h
+++ i2c/include/FreeRTOSConfig.h
@@ -89,7 +89,7 @@
 #define configUSE_IDLE_HOOK		0
 #define configUSE_TICK_HOOK		0
 #define configCPU_CLOCK_HZ		72000000
-#define configSYSTICK_CLOCK_HZ	configCPU_CLOCK_HZ / 8 /* vTaskDelay() fix */
+#define configSYSTICK_CLOCK_HZ	(configCPU_CLOCK_HZ >> 3) /* vTaskDelay() fix */
 #define configTICK_RATE_HZ		250
 #define configMAX_PRIORITIES		2
 #define configMINIMAL_STACK_SIZE	128
